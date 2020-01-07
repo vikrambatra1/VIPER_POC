283732caebc1c061f2f4215d536d40dd24b4d15b
Vikram Batra <vikram.batra1@globallogic.com>
2020-01-07 20:42:23 +0530

    Files modified

---
+++ VIPER_POC/VIPER_POC/FeedModule/View/FeedDetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FeedDetailsViewController.swift
+//  VIPER_POC
+//
+//  Created by Vikram Batra1 on 07/01/20.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import UIKit
+
+class FeedDetailsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
